7d266b0f81531dd05c116ccf764dc3e1c327d670
Patrick Connolly <patrick.c.connolly@gmail.com>
2017-02-01 19:31:05 -0500

    Use prettier date rather than meetup event id in filenames.

--- generate_csv.py
+++ generate_csv.py
@@ -5,14 +5,19 @@ import meetup.api
 import sys
 import os
 
+from datetime import datetime
+
 api_key = os.environ['MEETUP_API_KEY']
 script, event_id = sys.argv
 
 client = meetup.api.Client(api_key)
 
+event = client.GetEvent(id=event_id)
+date = datetime.fromtimestamp(event.time/1000).strftime('%Y-%m-%d')
+filename = 'meetup-rsvps-{}.csv'.format(date)
+
 response = client.GetRsvps(event_id=event_id)
 
-filename = 'meetup-rsvps-{}.csv'.format(event_id)
 with open(filename, 'w') as csvfile:
     writer = csv.writer(csvfile)
     writer.writerow(['name', 'photo_link'])
